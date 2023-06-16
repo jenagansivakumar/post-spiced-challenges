3bfe6a3d9323cc471d9f6baeb31ad4b8c6e94b92
Jena <jenagansivakumar@gmail.com>
2023-06-16 17:24:55 +0200

    added superhi

---
+++ superhi/intro-to-react/intro-to-react/src/App.jsx
@@ -0,0 +1,16 @@
+import "../src/styles.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <div>
+        <h1>Zesty</h1>
+        <h2>
+          A photography project <br /> by Ella Fieldling
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default App;
